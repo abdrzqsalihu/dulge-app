4f972d2e86f7b28c7097aa0c2cf83e530bdd4f2c
abdrzqsalihu <abdrzq.salihu@gmail.com>
2023-05-30 23:36:40 +0100

    added a custom port number

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // The port number you want to use
+    // host: 'your-desired-url', // The URL you want to use
+  },
 })
+
