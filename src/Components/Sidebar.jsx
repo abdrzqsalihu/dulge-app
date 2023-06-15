bd9ffc131d8e648370ec291d2f87294129e6a81d
abdrzqsalihu <abdrzq.salihu@gmail.com>
2023-06-15 16:39:14 +0100

    added some navigations to the mobilemenu

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import React, { useEffect } from 'react'
 import M from "materialize-css/dist/js/materialize.min.js";
 import Logo from '../assets/img/logo.png';
+import {UserAuth} from '../context/AuthContext';
 
 function Sidebar() {
     const link_color = {
@@ -13,6 +14,17 @@ function Sidebar() {
       let sidenav = document.querySelector("#slide-out");
       M.Sidenav.init(sidenav, {});
     }, []);
+
+    
+    const {user, logOut} = UserAuth();
+
+    const handleSignOut = async () => {
+        try {
+            await logOut()
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
   return (
   <div className='sidebar hide-on-large-only'>
@@ -33,6 +45,8 @@ function Sidebar() {
             <li><Link to="/" style={link_color} >Solution</Link></li>
             <li><Link to="/" style={link_color} >Pricing</Link></li>
             <li><Link to="/" style={link_color} >Resources</Link></li>   
+            <li>{user?.displayName ? ( <Link to="/Dashboard" className="btn login_btn z-depth-0 panel_btn" style={{border: "none !important"}}>Panel</Link> ) : ( <Link to="/signin"  className="btn login_btn z-depth-0">Log In</Link>)}</li>
+            <li>{user?.displayName ? ( <a href="/" className="btn start_btn z-depth-0" onClick={handleSignOut}>Logout</a> ) : (  <Link to="/"  className="btn start_btn z-depth-0">Start for Free</Link>)}</li>   
         </ul>
    
   </div>
