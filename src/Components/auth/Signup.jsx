0b4635dfb3865912408edd6fe886ef6ca2fbad98
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2023-10-30 12:53:06 +0100

    commented unused input field on the signup page

--- src/Components/auth/Signup.jsx
+++ src/Components/auth/Signup.jsx
@@ -101,10 +101,10 @@ function Signup() {
 
               <form onSubmit={handleSignUp}> <br />
                 {/* <h2>Login</h2> */}
-                <div className="input-container">
+                {/* <div className="input-container">
                   <input placeholder="Name" className="input-field" type="text" />
                   <span className="input-highlight"></span>
-                </div>
+                </div> */}
 
                 <div className="input-container">
                   <input placeholder="Email" className="input-field" type="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
