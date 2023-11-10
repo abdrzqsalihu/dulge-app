790ee507d7b35c27dfeaec31cfc2c1b995c3f100
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2023-11-10 22:28:33 +0100

    Update .gitignore to properly exclude .env files

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you need
+// Import the functions needed for the SDKs
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDAS4uyEa0d-LVDSCblu7E4BGkJN_tuE9Y",
-  authDomain: "dulge-fbc58.firebaseapp.com",
-  projectId: "dulge-fbc58",
-  storageBucket: "dulge-fbc58.appspot.com",
-  messagingSenderId: "299311719664",
-  appId: "1:299311719664:web:d5e0f7fef2956cb5b08e35"
+  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
