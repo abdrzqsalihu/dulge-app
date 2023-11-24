998aef3c01cc61129c198e5af2423c96c809dcfb
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2023-11-24 22:12:14 +0100

    added a funny gif to the dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { UserAuth } from "../context/AuthContext";
+import gif from "../assets/img/cooking.gif";
 
 function Dashboard() {
   const { user } = UserAuth();
@@ -25,7 +26,28 @@ function Dashboard() {
             {user.displayName || user.email}!
           </p>
         )}
-
+<hr style={{height: "1px", border: "none", backgroundColor: "#d3d3d3"}} />
+        <div style={{ margin: "auto" }} className="center">
+          {" "}
+          <br />
+          <br />
+          <h5 style={{ textShadow: "2px 2px 5px #1B89FA", fontSize: "1.8rem", fontWeight: "600" }}>
+            Omor I still dey cook!👨🏽‍🍳🍳
+          </h5>{" "}
+          <br />
+          <img
+            src={gif}
+            style={{ width: "94%" }}
+            className="hide-on-large-only"
+            alt=""
+          />
+          <img
+            src={gif}
+            style={{ width: "40%" }}
+            className="hide-on-med-and-down"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
